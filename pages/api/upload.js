fdf676b6af20ed84bd06f2ec4f4c6b650d399d83
oujingzhou <oujingzhou@hotmail.com>
2024-01-16 23:54:48 +0800

    first commit

---
+++ pages/api/upload.js
@@ -0,0 +1,97 @@
+import { createRouter } from 'next-connect'
+import multer from 'multer'
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai')
+
+const API_KEY = process.env.GEMINI_API_KEY
+const MODEL_NAME = 'gemini-pro-vision'
+
+const upload = multer({
+    storage: multer.memoryStorage()
+})
+
+const router = createRouter()
+
+router.use(upload.single('image'))
+
+router.all((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+    } else {
+        next()
+    }
+})
+router.post(async (req, res) => {
+    console.log('req.file:', req.file)
+    try {
+        console.log('Starting request...')
+        const genAI = new GoogleGenerativeAI(API_KEY)
+        const model = genAI.getGenerativeModel({ model: MODEL_NAME })
+
+        const generationConfig = {
+            temperature: 0.8,
+            topK: 32,
+            topP: 1,
+            maxOutputTokens: 4096
+        }
+
+        const safetySettings = [
+            {
+                category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            },
+            {
+                category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+                threshold: HarmBlockThreshold.BLOCK_NONE
+            }
+        ]
+
+        if (!req.file) {
+            console.log('No file uploaded.')
+            return res.status(400).json({ error: 'No file uploaded.' })
+        }
+
+        const parts = [
+            {
+                text: '作为一位熟练的儿童心理翻译员，我可以根据用户提供的照片来识别孩子的情绪和想法，并根据他们的肢体语言、表情和周围环境推测他们想说的话。以下是修改后的回答格式：如果图片中没有孩子：🧒：图片中没有孩子~,如果有孩子的话，我将以孩子的视角回答：🧒：<孩子的情绪>，<孩子的想法>'
+            },
+            {
+                inlineData: {
+                    mimeType: 'image/jpeg',
+                    data: req.file.buffer.toString('base64')
+                }
+            }
+        ]
+
+        const result = await model.generateContent({
+            contents: [{ role: 'user', parts }],
+            generationConfig,
+            safetySettings
+        })
+
+        if (!result) {
+            return res.status(502).json({ error: 'Bad Gateway' })
+        } else {
+            const responseText = result.response.text()
+            return res.status(200).json({ result: responseText })
+        }
+    } catch (error) {
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
+})
+
+export const config = {
+    api: {
+        bodyParser: false,
+        externalResolver: true
+    }
+}
+
+export default router.handler()
