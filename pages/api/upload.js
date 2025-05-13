d58a85f53fd5b6e88b1b45fc151f528e2697895f
oujingzhou <oujingzhou@stepfun.com>
2025-05-13 20:30:01 +0800

    update gemini model

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -3,7 +3,7 @@ import multer from 'multer'
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai')
 
 const API_KEY = process.env.GEMINI_API_KEY
-const MODEL_NAME = 'gemini-1.5-pro'
+const MODEL_NAME = 'gemini-2.0-flash'
 
 const upload = multer({
     storage: multer.memoryStorage()
